1a912e22e3e52489f50936922e3f1872416b4bc9
VasilyGorbunov <vasily.gorbunov@gmail.com>
2022-11-07 16:32:44 +0300

    lesson 13

---
+++ src/views/product/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Продукты</div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  mounted() {
+    $(document).trigger('change')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
